f8cb6e56e0456fda6c460fbf6d396cb08f467510
EnriqueArancon <enrique.arancon@alumnos.upm.es>
2026-09-18 20:09:04 +0200

    added GET /users endpoint to return all users #26

---
+++ src/main/java/es/upm/miw/devops/rest/UsersResource.java
@@ -0,0 +1,20 @@
+package es.upm.miw.devops.rest;
+
+import es.upm.miw.devops.code.User;
+import es.upm.miw.devops.code.UsersDatabase;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+public class UsersResource {
+
+    @GetMapping
+    public List<User> readAll() {
+        return new UsersDatabase().findAll().collect(Collectors.toList());
+    }
+}
