172c39b47936ee93fb269bb8a1dcd41ed8f0c3de
EnriqueArancon <enrique.arancon@alumnos.upm.es>
2026-09-20 12:03:40 +0200

    added  PUT /users/{id} endpoint without tests (#41)

--- src/main/java/es/upm/miw/devops/rest/UsersResource.java
+++ src/main/java/es/upm/miw/devops/rest/UsersResource.java
@@ -16,4 +16,25 @@ public class UsersResource {
     public List<User> readAll() {
         return new UsersDatabase().findAll().toList();
     }
+
+    @org.springframework.web.bind.annotation.PutMapping("/{id}")
+    public User update(@org.springframework.web.bind.annotation.PathVariable String id, @org.springframework.web.bind.annotation.RequestBody User user) {
+        return new UsersDatabase().findAll()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .map(u -> {
+                    u.setName(user.getName());
+                    u.setFamilyName(user.getFamilyName());
+                    u.setEmail(user.getEmail());
+                    u.setIdentity(user.getIdentity());
+                    u.setAddress(user.getAddress());
+                    u.setCity(user.getCity());
+                    u.setProvince(user.getProvince());
+                    u.setPostalCode(user.getPostalCode());
+                    u.setActive(user.isActive());
+                    u.setFractions(user.getFractions());
+                    return u;
+                })
+                .orElseThrow(() -> new org.springframework.web.server.ResponseStatusException(org.springframework.http.HttpStatus.NOT_FOUND, "User not found"));
+    }
 }
